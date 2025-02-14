336d3ab456347b8b90f6762be04960ec2456c008
neanvo <neanvo@protonmail.com>
2025-02-14 21:53:15 +0300

    is_wsl inprovement

--- nvim/lua/misc.lua
+++ nvim/lua/misc.lua
@@ -1,11 +1,18 @@
 local M = {}
 
-M.is_wsl = function ()
-  if os.getenv("WSL_DISTRO_NAME") then
-    return true
-  end
-  return false
-end
+M.is_wsl = function()
+	if os.getenv("WSL_DISTRO_NAME") then
+		return true
+	end
 
+	local file = io.open("/proc/version", "r")
+	if file then
+		local content = file:read("*a"):lower()
+		file:close()
+		return content:find("microsoft") ~= nil or content:find("wsl") ~= nil
+	end
+
+	return false
+end
 
 return M
